a97e10ec53c72915e5e8b9547d661be2748975b2
luffy <youssefnafaa529@gmail.com>
2023-07-20 02:05:14 +0100

    training

---
+++ lage.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int birthday, age ;
+        printf("write your birthday : ");
+        scanf("%d", &birthday);
+    age = 2023 -birthday;
+        printf(" your age is : %d\n", age);
+    return 0;
+}

54d32113309a6663d483bed4ca47f3ab729f0e6f
luffy <youssefnafaa529@gmail.com>
2023-07-22 14:57:09 +0100

    calculatrice

---
+++ calculatrice.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <conio.h>
+
+int main()
+{
+    float A, B, S, P, Df, Dv;
+        printf("entrer la valeur de A : ");
+        scanf("%f", &A);
+        printf("entrer la valeur de B : ");
+        scanf("%f", &B);
+    S = A + B;
+    P = A * B;
+    Df = A - B;
+    Dv = A / B;
+        printf("A + B = %.2f\n",S);
+        printf("A * B = %.2f\n",P);
+        printf("A - B = %.2f\n",Df);
+        printf("A / B = %.2f\n",Dv);
+        getch();
+
+
+    return 0;
+}

51bd65c9943eb78271b4558997c7aa2127a61d5f
luffy <youssefnafaa529@gmail.com>
2023-07-22 19:34:41 +0100

    some_moyenne

---
+++ some_moyenne.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>3
+
+int main()
+{
+    float N1, N2, N3, N4, N5;
+    float Somme, moyenne;
+        printf("entrer les 5 notes : ");
+        scanf("%f %f %f %f %f", &N1, &N2, &N3, &N4, &N5);
+    Somme = N1 + N2 + N3 + N4 + N5;
+    moyenne = (Somme) / 5;
+        printf("La somme is : %.2f\n",Somme);
+        printf("Le moyeene is %.2f\n",moyenne);
+
+
+    return 0;
+}

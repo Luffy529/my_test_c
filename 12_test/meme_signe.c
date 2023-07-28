049a1603592eaae24bf142ee571ee420b7f48c3a
Luffy529 <youssefnafaa529@gmail.com>
2023-07-28 20:12:38 +0100

    Update

---
+++ 12_test/meme_signe.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A, B;
+        printf("entrer la valeur A : ");
+        scanf("%d", &A);
+        printf("entrer la valeur B : ");
+        scanf(" %d", &B);
+    if (A*B>0)
+        printf("A et B ont la meme singe");
+    else printf("A et B ont deux signes differents");
+
+
+
+
+
+    return 0;
+}

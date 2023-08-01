53d797d9243d6673600ff6c7cb80512f8925fdfa
Luffy529 <youssefnafaa529@gmail.com>
2023-08-01 19:01:36 +0100

    up

---
+++ loops/miltiple.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main ()
+{
+    int M , i;
+        for (i=0; i <= 10; i++)
+    {
+        M = 7* i;
+            printf ("7 * %d = %d\n",i , M);
+    }
+
+        return 0;
+}

93026c27045c8733cb3ecb55ea39fbe38fec5984
Luffy529 <youssefnafaa529@gmail.com>
2023-08-02 14:56:16 +0100

    while

---
+++ loops/while.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main ()
+{
+    int N , i;
+    printf ("entrer un nombre : ");
+    scanf ("%d",&N);
+
+        while (N<0 || N>10)
+    {
+    printf ("entrer un nombre : ");
+    scanf ("%d",&N);
+    }
+    i=0;
+    while(i<=10)
+    {
+    printf ("%d x %d = %d \n",i,N,i*N);
+    i++;
+    }
+        return 0;
+}

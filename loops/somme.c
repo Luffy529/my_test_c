394092156a4c81ec7e246dcbfd908a3f2225c750
Luffy529 <youssefnafaa529@gmail.com>
2023-08-02 13:29:35 +0100

    up

---
+++ loops/somme.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main ()
+{
+    int S , i;
+    S = 0;
+    for (i=1; i <= 20;i++) 
+    {
+    S=S+i;
+    }
+        printf("la somme des 20 premies entiers est : %d\n", S);
+    
+
+
+
+
+    return 0;
+}

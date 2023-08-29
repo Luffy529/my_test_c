f03b2489205a167d4f3a74d5704495be21802ccb
Luffy529 <youssefnafaa529@gmail.com>
2023-08-29 18:32:07 +0100

    up

---
+++ les fonction/2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+int N = 6;
+/**
+ * puissance - Calculates the square of an integer.
+ *
+ * This function takes an integer N as a parameter and calculates
+ * the square of N.
+ *
+ * @param N The integer whose square is to be calculated.
+ * @return The square of the input integer.
+ */
+
+	int puissance(int N)
+	{
+		int P;
+		P = N * N;
+
+return (P);
+	}
+/**
+* puissance - Calculates the square of an integer.
+ *
+ * This function takes an integer N as a parameter and calculates
+ * the square of N.
+ * Description : main function
+ * @param N The integer whose square is to be calculated.
+ * @return The square of the input integer.
+ */
+
+	int main(void)
+	{
+	
+		int result;
+		result = puissance(N);
+		printf("%d\n", result);
+		return (0);
+	}
